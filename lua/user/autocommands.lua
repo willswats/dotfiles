cda1d53b25142b8c3cb0a3a85440e09aebcd462a
willswats <williamstuwatson@gmail.com>
2023-02-14 00:37:50 +0000

    Remove autocmds

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -1,32 +1,4 @@
 vim.cmd [[
-  augroup general_settings
-    autocmd!
-    autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR>
-    autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Visual', timeout = 200})
-    autocmd BufWinEnter * :set formatoptions-=cro
-    autocmd FileType qf set nobuflisted
-  augroup end
-
-  augroup git
-    autocmd!
-    autocmd FileType gitcommit setlocal wrap
-  augroup end
-
-  augroup markdown
-    autocmd!
-    autocmd FileType markdown setlocal wrap
-  augroup end
-
-  augroup auto_resize
-    autocmd!
-    autocmd VimResized * tabdo wincmd =
-  augroup end
-
-  augroup alpha
-    autocmd!
-    autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
-  augroup end
-
   augroup format
     autocmd!
     autocmd BufWritePre * lua vim.lsp.buf.format({ bufnr = bufnr })
@@ -37,4 +9,9 @@ vim.cmd [[
     autocmd TermOpen * setlocal nonumber
     autocmd TermOpen * normal a
   augroup END
+
+augroup illuminate
+    autocmd!
+    autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Visual', timeout = 200})
+  augroup end
 ]]
