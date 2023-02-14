dbf2889aca9ee3284222d8d23b4dc34b33584525
willswats <williamstuwatson@gmail.com>
2023-02-14 15:12:33 +0000

    Update lualine seperator

--- lua/user/plugins/lualine.lua
+++ lua/user/plugins/lualine.lua
@@ -16,7 +16,7 @@ lualine.setup({
   options = {
     icons_enabled = true,
     theme = "auto",
-    component_separators = { left = "", right = "" },
+    component_separators = { left = icons.ui.LineMiddle, right = icons.ui.LineMiddle },
     section_separators = { left = "", right = "" },
     disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
     always_divide_middle = true,
