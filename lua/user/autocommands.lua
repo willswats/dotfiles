6f7ae33614b91f3b180da96f266cdf1ca1795eae
willswats <williamstuwatson@gmail.com>
2023-02-14 09:24:49 +0000

    Add back autocmd

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -1,17 +1,22 @@
 vim.cmd [[
-  augroup format
-    autocmd!
-    autocmd BufWritePre * lua vim.lsp.buf.format({ bufnr = bufnr })
-  augroup end
-
   augroup terminal
     autocmd!
     autocmd TermOpen * setlocal nonumber
     autocmd TermOpen * normal a
   augroup END
 
-augroup illuminate
+  augroup format
+    autocmd!
+    autocmd BufWritePre * lua vim.lsp.buf.format({ bufnr = bufnr })
+  augroup end
+
+  augroup illuminate
     autocmd!
     autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Visual', timeout = 200})
+  augroup end
+
+  augroup alpha
+    autocmd!
+    autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
   augroup end
 ]]
