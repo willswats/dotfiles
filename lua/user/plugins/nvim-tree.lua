e870403e75921cfb453eddd9c42420fb1ac698ac
willswats <williamstuwatson@gmail.com>
2023-02-17 01:48:49 +0000

    Set nvim-tree sync_root true

--- lua/user/plugins/nvim-tree.lua
+++ lua/user/plugins/nvim-tree.lua
@@ -16,9 +16,10 @@ end
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvim_tree.setup {
+  sync_root_with_cwd = true,
   update_focused_file = {
     enable = true,
-    update_cwd = true,
+    update_root = true,
   },
   renderer = {
     root_folder_modifier = ":t",
