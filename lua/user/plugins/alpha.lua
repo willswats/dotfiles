7135a2b725a2d964514b1bcfe5c70ad42ab7c212
willswats <williamstuwatson@gmail.com>
2023-02-19 11:22:16 +0000

    Re-order dashboard

--- lua/user/plugins/alpha.lua
+++ lua/user/plugins/alpha.lua
@@ -14,11 +14,9 @@ if not status_ok_icons then
 end
 
 dashboard.section.buttons.val = {
-  dashboard.button("f", icons.ui.FindFile .. "  " .. "Find File", ":Telescope find_files <CR>"),
   dashboard.button("n", icons.ui.NewFile .. "  " .. "New file", ":ene <BAR> startinsert <CR>"),
-  dashboard.button("p", icons.ui.Project .. "  " .. "Projects", ":Telescope projects <CR>"),
   dashboard.button("r", icons.ui.Files .. "  " .. "Recent files", ":Telescope oldfiles <CR>"),
-  dashboard.button("t", icons.ui.FindText .. "  " .. "Find text", ":Telescope live_grep <CR>"),
+  dashboard.button("p", icons.ui.Project .. "  " .. "Projects", ":Telescope projects <CR>"),
   dashboard.button("c", icons.ui.Gear .. "  " .. "Configuration", ":e $MYVIMRC <CR>"),
   dashboard.button("q", icons.ui.BoldClose .. "  " .. "Quit", ":qa<CR>"),
 }
