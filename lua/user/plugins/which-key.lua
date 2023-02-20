8128f9909d939a2060da2d0257cbda1968bd988e
willswats <williamstuwatson@gmail.com>
2023-02-20 17:20:21 +0000

    Update whichkey binding

--- lua/user/plugins/which-key.lua
+++ lua/user/plugins/which-key.lua
@@ -173,7 +173,7 @@ local mappings = {
     name = "Git",
     j = { "<cmd>lua require 'gitsigns'.next_hunk({navigation_message = false})<cr>", "Next hunk" },
     k = { "<cmd>lua require 'gitsigns'.prev_hunk({navigation_message = false})<cr>", "Prev hunk" },
-    l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
+    b = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
     p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview hunk" },
     r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset hunk" },
     R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset buffer" },
