7fac9227a3b4cbb441c6c2466db8eb59ff943e82
willswats <williamstuwatson@gmail.com>
2023-02-21 14:53:57 +0000

    Update lualine config

--- lua/user/plugins/lualine.lua
+++ lua/user/plugins/lualine.lua
@@ -43,7 +43,7 @@ lualine.setup({
   sections = {
     lualine_a = { "mode" },
     lualine_b = { branch, diff, diagnostics },
-    lualine_c = {},
+    lualine_c = { "filename" },
     lualine_x = { "filetype" },
     lualine_y = { "progress" },
     lualine_z = { "location" }
