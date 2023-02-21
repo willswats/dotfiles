56c8d75772a522fc48317027a446f594cdf9d8e3
willswats <williamstuwatson@gmail.com>
2023-02-21 13:49:25 +0000

    Add to project.lua config

--- lua/user/plugins/project.lua
+++ lua/user/plugins/project.lua
@@ -25,7 +25,7 @@ project.setup({
   -- detection_methods = { "lsp", "pattern" }, -- NOTE: lsp detection will get annoying with multiple langs in one project
   detection_methods = { "pattern" },
   ---@usage patterns used to detect root dir, when **"pattern"** is in detection_methods
-  patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", ".project.nvim" },
+  patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", ".csproj", ".project.nvim" },
   ---@ Show hidden files in telescope when searching for files in a project
   show_hidden = false,
   ---@usage When set to false, you will get a message when project.nvim changes your directory.
