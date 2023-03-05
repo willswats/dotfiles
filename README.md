d5e594fcbc897ce85cc982597ef418a209d4f1a8
willswats <williamstuwatson@gmail.com>
2023-03-05 13:42:59 +0000

    Update install script

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # dotfiles
 
 My dotfiles.
+
+## Prerequisites
+
+- `gio`
+
+## General Information
+
+The install script will send old configs to the trash, and will symlink the configs from this directory.
--- install.sh
+++ install.sh
@@ -1,7 +1,28 @@
-ln -s "$(pwd)/alacritty" ~/.config/
+# Alacritty
+gio trash ~/.config/alacritty
+mkdir ~/.config/alacritty
+ln -s "$(pwd)/alacritty/alacritty.yml" ~/.config/alacritty
+git clone https://github.com/catppuccin/alacritty.git ~/.config/alacritty/catppuccin
+
+# Neovim
+gio trash ~/.config/nvim
 ln -s "$(pwd)/nvim" ~/.config/
-ln -s "$(pwd)/fish" ~/.config/
+
+# fish
+gio trash ~/.config/fish
+mkdir ~/.config/fish
+ln -s "$(pwd)/fish/config.fish" ~/.config/fish/
+
+# autostart
+gio trash ~/.config/autostart/rclone
 ln -s "$(pwd)/autostart/rclone" ~/.config/autostart/
+gio trash ~/.config/autostart/OpenTabletDriver.Daemon
 ln -s "$(pwd)/autostart/OpenTabletDriver.Daemon" ~/.config/autostart/
+
+# bookmarks
+gio trash ~/.config/gtk-3.0/bookmarks
 ln -s "$(pwd)/gtk-3.0/bookmarks" ~/.config/gtk-3.0/
+
+# fonts
+gio trash ~/.local/share/fonts
 ln -s "$(pwd)/fonts" ~/.local/share/
