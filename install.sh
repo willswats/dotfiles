0c086a07de133f0a7bb41405347c8c3afe314c49
willswats <williamstuwatson@gmail.com>
2023-03-05 12:50:55 +0000

    Add install.sh and more dotfiles

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 ln -s "$(pwd)/alacritty" ~/.config/
 ln -s "$(pwd)/nvim" ~/.config/
 ln -s "$(pwd)/fish" ~/.config/
+ln -s "$(pwd)/autostart/rclone" ~/.config/autostart/
+ln -s "$(pwd)/autostart/OpenTabletDriver.Daemon" ~/.config/autostart/
+ln -s "$(pwd)/gtk-3.0/bookmarks" ~/.config/gtk-3.0/
+ln -s "$(pwd)/fonts" ~/.local/share/
