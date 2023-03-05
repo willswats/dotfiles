2e637ef19f65af0d09278695ee6e1b4f77dcf43b
willswats <williamstuwatson@gmail.com>
2023-03-05 13:48:24 +0000

    Add alacritty and fish configs

---
+++ fish/config.fish
@@ -0,0 +1,17 @@
+# Remove fish greeting
+set -g fish_greeting
+
+# Set alias
+alias flatdel="flatpak uninstall --delete-data"
+alias main="distrobox enter main -- fish"
+alias docker="podman"
+alias nv="nvim"
+alias lg="lazygit"
+
+# set PATH
+set PATH $HOME/.cargo/bin $PATH # Set PATH for cargo
+set PATH $HOME/.local/share/pnpm $PATH # Set PATH for pnpm
+
+# node version on cd
+fnm env --use-on-cd | source
+
