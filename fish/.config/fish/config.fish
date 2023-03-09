73d94c4d0a448c3c2323c31590d30f88b8dd0a9e
willswats <williamstuwatson@gmail.com>
2023-03-09 01:26:07 +0000

    Move theme

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,9 +1,6 @@
 # Remove fish greeting
 set -g fish_greeting
 
-# Set theme
-fish_config theme choose "Catppuccin Mocha"
-
 # Set alias
 alias flatdel="flatpak uninstall --delete-data"
 alias main="distrobox enter main -- fish"
@@ -14,6 +11,9 @@ alias lg="lazygit"
 # set PATH
 set PATH $HOME/.cargo/bin $PATH # Set PATH for cargo
 set PATH $HOME/.local/share/pnpm $PATH # Set PATH for pnpm
+
+# Set theme
+fish_config theme choose "Catppuccin Mocha"
 
 # node version on cd
 fnm env --use-on-cd | source
