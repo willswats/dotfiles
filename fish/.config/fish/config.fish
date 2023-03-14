c82807a4c8d62cb9f464b9301764b7e7acda9c5b
willswats <williamstuwatson@gmail.com>
2023-03-14 00:52:04 +0000

    Edit config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,14 +4,26 @@ set -g fish_greeting
 # Set alias
 alias flatdel="flatpak uninstall --delete-data"
 alias main="distrobox enter main -- fish"
-alias key="keychain --quiet --agents ssh id_ed25519"
 alias docker="podman"
+
 # set PATH
-set PATH $HOME/.cargo/bin $PATH # Set PATH for cargo
-set PATH $HOME/.local/share/pnpm $PATH # Set PATH for pnpm
+set PATH $HOME/.cargo/bin $PATH
+set PATH $HOME/.local/share/pnpm $PATH
 
 # Set theme
 fish_config theme choose "Catppuccin Mocha"
 
 # node version on cd
 fnm env --use-on-cd | source
+
+# keychain
+if status --is-interactive
+    keychain --quiet --agents ssh id_ed25519
+end
+
+begin
+    set -l HOSTNAME (hostname)
+    if test -f ~/.keychain/$HOSTNAME-fish
+        source ~/.keychain/$HOSTNAME-fish
+    end
+end
